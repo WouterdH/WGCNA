69fc96c5d68734dfe195e35c3d61e558bf537acb
Wouter den Hollander <Wouter@Wouters-MacBook-Pro.local>
2017-08-22 13:51:37 +0200

    INVIVO_DM_LIVER.sh

---
+++ IN_VIVO/DM/LIVER/INVIVO_DM_LIVER.sh
@@ -0,0 +1,1 @@
+Rscript ~/gitProjects/WGCNA/0_extractZIP.R /data/wouter/WGCNA/OUTPUT/IN_VIVO/DM/LIVER/ /data/wouter/WGCNA/DATA/anonftp.niehs.nih.gov/drugmatrix/Affymetrix_data/Bulk_data_by_organ/LIVER/
