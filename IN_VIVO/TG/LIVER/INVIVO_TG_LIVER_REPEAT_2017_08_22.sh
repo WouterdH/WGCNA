430ea1d9b83fa82b01d4e1cb1333ff197e451336
Wouter den Hollander <Wouter@Wouters-MacBook-Pro.local>
2017-08-22 14:37:46 +0200

    run 1_readCEL_runRMA.R on DM, TG & DMTG

--- IN_VIVO/TG/LIVER/INVIVO_TG_LIVER_REPEAT_2017_08_22.sh
+++ IN_VIVO/TG/LIVER/INVIVO_TG_LIVER_REPEAT_2017_08_22.sh
@@ -1,1 +1,3 @@
-Rscript ~/gitProjects/WGCNA/0_extractZIP.R /data/wouter/WGCNA/OUTPUT/IN_VIVO/TG/LIVER/REPEAT/ /data/wouter/WGCNA/DATA/ftp.biosciencedbc.jp/archive/open-tggates/LATEST/Rat/in_vivo/Liver/Repeat/
+# Rscript ~/gitProjects/WGCNA/0_extractZIP.R /data/wouter/WGCNA/OUTPUT/IN_VIVO/TG/LIVER/REPEAT/ /data/wouter/WGCNA/DATA/ftp.biosciencedbc.jp/archive/open-tggates/LATEST/Rat/in_vivo/Liver/Repeat/
+
+Rscript ~/gitProjects/WGCNA/1_readCEL_runRMA.R /data/wouter/WGCNA/OUTPUT/IN_VIVO/TG/LIVER/REPEAT/ rat2302rnentrezgcdf /data/wouter/WGCNA/OUTPUT/IN_VIVO/TG/LIVER/REPEAT/
--- IN_VIVO/TG/LIVER/INVIVO_TG_LIVER_SINGLE_2017_08_22.sh
+++ IN_VIVO/TG/LIVER/INVIVO_TG_LIVER_SINGLE_2017_08_22.sh
@@ -1,1 +1,3 @@
-Rscript ~/gitProjects/WGCNA/0_extractZIP.R /data/wouter/WGCNA/OUTPUT/IN_VIVO/TG/LIVER/SINGLE/ /data/wouter/WGCNA/DATA/ftp.biosciencedbc.jp/archive/open-tggates/LATEST/Rat/in_vivo/Liver/Single/
+# Rscript ~/gitProjects/WGCNA/0_extractZIP.R /data/wouter/WGCNA/OUTPUT/IN_VIVO/TG/LIVER/SINGLE/ /data/wouter/WGCNA/DATA/ftp.biosciencedbc.jp/archive/open-tggates/LATEST/Rat/in_vivo/Liver/Single/
+
+Rscript ~/gitProjects/WGCNA/1_readCEL_runRMA.R /data/wouter/WGCNA/OUTPUT/IN_VIVO/TG/LIVER/SINGLE/ rat2302rnentrezgcdf /data/wouter/WGCNA/OUTPUT/IN_VIVO/TG/LIVER/SINGLE/
